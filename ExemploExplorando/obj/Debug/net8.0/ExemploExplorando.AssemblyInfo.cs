f50c5843e041bd33e75a0782d9030befbd05f75d
John Wesley <89667722+johnwesleysa@users.noreply.github.com>
2024-08-17 23:40:49 -0300

    Desafio 03 finalizado
    Desafio 03 finalizado

--- ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0f54a7e123a59746ae0c4e8493d8d7edf634c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2354e2698f4ee3ce97888474f2972b683bab8827")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
