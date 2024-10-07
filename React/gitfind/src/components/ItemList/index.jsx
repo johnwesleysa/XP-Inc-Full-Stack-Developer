17335edb96d71e500c809de186e23139a5afd49b
John Wesley <john.wesley.alencar2@gmail.com>
2024-10-07 09:30:40 -0300

    Projeto GitFind Concluído, o site tem como intuito realizar buscas na api do gitgub, passando como parâmetro o login, e retortar o nome do usuário, imagem de perfil, descrição, assim como todos os repositórios públicos com seus títulos, descrições e link para o repositório.

--- React/gitfind/src/components/ItemList/index.jsx
+++ React/gitfind/src/components/ItemList/index.jsx
@@ -1,7 +1,12 @@
 import './styles.css'
 
-function ItemList () {
-    return <div>index</div>
+function ItemList ({title, description, link}) {
+    return <div className="item-list">
+        <a href={link} target='blank'><strong>{title}</strong></a>
+        <p>{description}</p>
+        <hr/>
+        
+    </div>
 }
 
 export default ItemList
--- React/gitfind/src/pages/home/index.js
+++ React/gitfind/src/pages/home/index.js
@@ -1,8 +1,34 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
 import background from "../../assets/background.png"
 import './styles.css'
+import ItemList from "../../components/ItemList";
 
 function App() {
+  const [user, setUser] = useState('')
+  const [currentUser, setCurrentUser] = useState(null)
+  const [repos, setRepos] = useState(null)
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`)
+    const newUser = await userData.json()
+    
+    if(newUser.name){
+      const {avatar_url, name, bio, login} = newUser;
+      setCurrentUser({avatar_url, name, bio, login})
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`)
+      const newRepos = await reposData.json()
+
+      if(newRepos.length) {
+        setRepos (newRepos)
+      }
+    }
+
+    
+  }
+
+
   return (
     <div className="App">
       <Header/>
@@ -10,22 +36,36 @@ function App() {
         <img src={background} className="background" alt="background app"/>
         <div className="info">
           <div className="">
-              <input name="usuario" placeholder="@username"/>
-              <button>Buscar</button>
+              <input 
+              name="usuario" 
+              value={user} onChange={event => setUser(event.target.value)} 
+              placeholder="@username"
+              />
+              <button onClick={handleGetData}>Buscar</button>
           </div>
-          <div className="perfil">
-              <img src="https://avatars.githubusercontent.com/u/89667722?v=4" className="profile" alt="Imagem de Perfil"/>
+          {currentUser?.name ? (<>
+            <div className="perfil">
+              <img src={currentUser.avatar_url} className="profile" alt="Imagem de Perfil"/>
               <div>
-                <h3>John Alencar</h3>
-                <span>@johnwesleysa</span>
+                <h3>{currentUser.name}</h3>
+                <span>@{currentUser.login}</span>
                 <br/>
-                <p>Prof. de TI experiente em otimização, suporte e automação com Python. Aprimorando Habilidades em ADS e engenharia e análise de dados</p>
+                <p>{currentUser.bio}</p>
               </div>
           </div>
           <hr/>
-          <div>
+          </>):null}
+          {repos?.length ? (<>
+            <div>
             <h4>Repositórios</h4>
+            {repos.map(repo => (
+              <ItemList title={repo.name} description={repo.description} link={repo.html_url}/>
+            ))}
+            
+            
           </div>
+          </>):null }
+          
         </div>
       </div>
     </div>
