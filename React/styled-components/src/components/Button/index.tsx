6c04e52297cacd6aeec92ce04faafc9d65652660
John Wesley <john.wesley.alencar2@gmail.com>
2024-11-19 15:52:39 -0300

    foi adicionado uma parte para renomear pastas e uma aba para visualizar as imagens dentro do proproio site, sem precisar do stach

--- React/styled-components/src/components/Button/index.tsx
+++ React/styled-components/src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ButtonContainer } from "./styles";
-
+import { IButton } from './types'
 const Button = ({title, variant = "primary", onClick}) => {
     return(
         <ButtonContainer variant={variant} title={title} onClick={onClick}>
